4c8c68d7ec96a04d2b6d9e97dd082b57740b48ee
Jo Falck-Ytter <jofa@stud.ntnu.no>
2022-02-08 11:56:03 +0100

    Issue #0
    fix: gjorde om navn for å passe prosjekt

--- matnett/wsgi.py
+++ matnett/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for files project.
+WSGI config for matnett project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'files.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matnett.settings')
 
 application = get_wsgi_application()
