fc5472e27ced663b8ec2dd44e5110647ee68435b
Rebeccca Pösche Ruud <rebeccaruuud@gmail.com>
2022-02-15 10:29:59 +0100

    Side for a legge til oppskrifter og koblet til url
     (#5)

--- matapp/views.py
+++ matapp/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-
 def browse(req):
     return render(req, 'matapp/browse.html')
+
 
 def myrecipes(request):
     return render(request, 'matapp/myrecipes.html')
 
+
 def profile(request):
     return render(request, 'matapp/profile.html')
+
 
 def alert(request):
     return render(request, 'matapp/alert.html')
 
+
 def recipe(request):
     return render(request, 'matapp/recipe.html')
+
+
+def addRecipe(request):
+    return render(request, 'matapp/addRecipe.html')
