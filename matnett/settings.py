e2bacab44a10dde323d5620f4d25a07cff12a019
Jakob Keller <jekeller@stud.ntnu.no>
2022-02-23 14:39:58 +0100

    Brukere kan laste opp en bilde til oppskriften sin

--- matnett/settings.py
+++ matnett/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 
 ]
 
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+ 
