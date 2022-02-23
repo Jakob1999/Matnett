ed4f2e36c03a19e169d3c82f9c66696d46691044
Jo Falck-Ytter <jofa@stud.ntnu.no>
2022-02-23 11:56:36 +0100

    test: tittel
    Første test som sjekker henting av tittel
    
    issue #11

--- matapp/tests.py
+++ matapp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Recipe
+from django.utils import timezone
 
 # Create your tests here.
+
+
+def create_recipe(title, description, ingredients, date_created):
+    """Lager en oppskrift som brukes i testingen
+
+    Args:
+        title (Charfield): Tittel/navn på retten
+        description (Textfield): fremgangsmåte
+        ingredients (Textfield): ingredienser
+        date_created (DateTimeField):
+    """
+    return Recipe.objects.create(title = title, ingredients = ingredients,
+                                 date_created=date_created, description = description)
+
+class CreateRecipe(TestCase):
+    def test_add_recipe(self):
+        recipe = create_recipe('pizza','Sett på ovn\nRull ut deig\nTa på fyll\nStrø over ost\nStek'
+                               ,'Pizzadeig, tomatsaus, skinke, ost',timezone.now())
+        self.assertEqual(recipe.title,'pizza')
