caa7fcd3dbc0899aeff500cd7d2a7c7b5cf8cc35
Jo Falck-Ytter <jofa@stud.ntnu.no>
2022-02-24 14:31:26 +0100

    test: endring oppskrift
    endrer direkte variablene, ikke gjennom en funksjon
    
    issue #11

--- matapp/tests.py
+++ matapp/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from .models import Recipe
 from django.utils import timezone
+import datetime
 
 # Create your tests here.
 
+CONST_PIZZA_TODO = 'Sett på ovn\nRull ut deig\nTa på fyll\nStrø over ost\nStek'
+CONST_PIZZA_ITEM = 'Pizzadeig, tomatsaus, skinke, ost'
 
 def create_recipe(title, description, ingredients, date_created):
     """Lager en oppskrift som brukes i testingen
@@ -19,9 +22,35 @@ def create_recipe(title, description, ingredients, date_created):
 
 class CreateRecipe(TestCase):
     def test_add_recipe(self):
-        recipe = create_recipe('pizza','Sett på ovn\nRull ut deig\nTa på fyll\nStrø over ost\nStek'
-                               ,'Pizzadeig, tomatsaus, skinke, ost',timezone.now())
+        """
+        Sjekker at tittel og andre deler i modellen oppfører seg som forventet.
+        TODO
+        Burde lage test som baserer seg på PK?
+        """
+        time = timezone.localdate()
+        recipe = create_recipe('pizza', CONST_PIZZA_TODO, CONST_PIZZA_ITEM,time)
+        recipe_two = create_recipe('taco','Stek kjøttdeig og krydder\nKutt opp grønnsaker\nS',
+                                   'Kjøttdeig, salat, ost, tortilla',time)
         self.assertEqual(recipe.title,'pizza')
-        self.assertEqual(recipe.description, 'Sett på ovn\nRull ut deig\nTa på fyll\nStrø over ost\nStek')
-        self.assertEqual(recipe.ingredients,'Pizzadeig, tomatsaus, skinke, ost')
-        self.assertEqual(recipe.date_created, timezone.now())
+        self.assertEqual(recipe_two.title,'taco')
+        self.assertEqual(recipe.description, CONST_PIZZA_TODO)
+        self.assertEqual(recipe.ingredients, CONST_PIZZA_ITEM) 
+        self.assertEqual(recipe.date_created, time)
+        self.assertNotEqual(recipe.title,'taco')
+        
+    def test_edit_recipe(self):
+        """
+        Endring av en recipe. 
+        """
+        time = timezone.localdate()
+        recipe = create_recipe('pizza', CONST_PIZZA_TODO ,CONST_PIZZA_ITEM,time)
+        recipe_two = create_recipe('taco','Stek kjøttdeig og krydder\nKutt opp grønnsaker\nS',
+                                   'Kjøttdeig, salat, ost, tortilla',time)
+        self.assertEqual(recipe.title,'pizza')
+        recipe.title = 'Mafiapannekake'
+        recipe_two.desciption = 'Alle kan lage taco'
+        self.assertNotEqual(recipe.title,'pizza')
+        self.assertEqual(recipe_two.desciption, 'Alle kan lage taco')
+        
+    #def test_delete_recipe(self):
+        
