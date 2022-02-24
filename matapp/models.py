675d95d1402568f24400201527332285776f66dd
Jo Falck-Ytter <jofa@stud.ntnu.no>
2022-02-24 14:29:59 +0100

    refactor: endre type i db
    fra datetimefield til datefield

--- matapp/models.py
+++ matapp/models.py
@@ -16,7 +16,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
     ingredients = models.TextField(null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_created = models.DateField(auto_now_add=True, null=True)
     
     def __str__(self):
         return self.title
